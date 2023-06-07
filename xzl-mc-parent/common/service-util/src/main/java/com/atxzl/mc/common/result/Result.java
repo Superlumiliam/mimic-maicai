ee5118e591b9bc61a95b6ffe157edad282ed5694
Superluminaliam <superluminaliam@gmail.com>
2023-06-07 07:07:23 -0700

    Result Code Enum

---
+++ xzl-mc-parent/common/service-util/src/main/java/com/atxzl/mc/common/result/Result.java
@@ -0,0 +1,39 @@
+package com.atxzl.mc.common.result;
+
+import lombok.Data;
+
+@Data
+public class Result<T> {
+
+    //status code
+    private Integer code;
+
+    //message
+    private String message;
+
+    //data
+    private T data;
+
+    private Result(){ }
+
+    //
+    public static<T> Result<T> build(T data, ResultCodeEnum resultCodeEnum){
+        Result<T> result = new Result<>();
+        //if response need data
+        if(data != null ){
+            result.setData(data);
+        }
+        result.setCode(resultCodeEnum.getCode());
+        result.setMessage(result.getMessage());
+        return result;
+    }
+
+    public static<T> Result<T> ok(T data){
+        Result<T> result = build(data, ResultCodeEnum.SUCCESS);
+        return result;
+    }
+    public static<T> Result<T> fail(T data){
+        Result<T> result = build(data, ResultCodeEnum.FAIL);
+        return result;
+    }
+}
